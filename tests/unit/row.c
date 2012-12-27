1da205d15135bd01e981d2a755c74e20cc653a46
Andrew Hutchings <andrew@linuxjedi.co.uk>
2012-12-27 22:09:42 +0000

    Add row function tests

---
+++ tests/unit/row.c
@@ -0,0 +1,157 @@
+/*  vim:expandtab:shiftwidth=2:tabstop=2:smarttab: 
+ *
+ *  Drizzle Client & Protocol Library
+ *
+ * Copyright (C) 2012 Andrew Hutchings (andrew@linuxjedi.co.uk)
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ *
+ *     * Redistributions in binary form must reproduce the above
+ * copyright notice, this list of conditions and the following disclaimer
+ * in the documentation and/or other materials provided with the
+ * distribution.
+ *
+ *     * The names of its contributors may not be used to endorse or
+ * promote products derived from this software without specific prior
+ * written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
+#include <libdrizzle-5.1/libdrizzle.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#ifndef EXIT_SKIP
+# define EXIT_SKIP 77
+#endif
+
+int main(int argc, char *argv[])
+{
+  (void) argc;
+  (void) argv;
+  drizzle_st *con;
+  drizzle_return_t ret;
+  drizzle_result_st *result;
+  drizzle_row_t row;
+  int num_fields;
+
+  con = drizzle_create_tcp("localhost", 3306, "root", "", "libdrizzle", 0);
+  if (con == NULL)
+  {
+    printf("Drizzle connection object creation error\n");
+    return EXIT_FAILURE;
+  }
+  ret = drizzle_connect(con);
+  if (ret != DRIZZLE_RETURN_OK)
+  {
+    printf("Drizzle connection failure\n");
+    return EXIT_SKIP;
+  }
+
+  drizzle_query_str(con, "create table libdrizzle.t1 (a int)", &ret);
+  if (ret != DRIZZLE_RETURN_OK)
+  {
+    printf("Create table failure\n");
+    return EXIT_FAILURE;
+  }
+
+  drizzle_query_str(con, "insert into libdrizzle.t1 values (1),(2),(3)", &ret);
+  if (ret != DRIZZLE_RETURN_OK)
+  {
+    printf("Insert failure\n");
+    return EXIT_FAILURE;
+  }
+
+  result= drizzle_query_str(con, "select * from libdrizzle.t1", &ret);
+  if (ret != DRIZZLE_RETURN_OK)
+  {
+    printf("Select failure\n");
+    return EXIT_FAILURE;
+  }
+  drizzle_result_buffer(result);
+  num_fields= drizzle_result_column_count(result);
+
+  if (num_fields != 1)
+  {
+    printf("Retrieved bad number of fields\n");
+    return EXIT_FAILURE;
+  }
+  row = drizzle_row_next(result);
+  if (row == NULL)
+  {
+    printf("Could not get the next row\n");
+    return EXIT_FAILURE;
+  }
+  
+  if (strcmp(row[0], "1") != 0)
+  {
+    printf("Retrieved bad next row value\n");
+    return EXIT_FAILURE;
+  }
+  drizzle_row_seek(result, 3);
+  row = drizzle_row_prev(result);
+  if (row == NULL)
+  {
+    printf("Could not get prev row\n");
+    return EXIT_FAILURE;
+  }
+  if (strcmp(row[0], "3") != 0)
+  {
+    printf("Retrieved bad prev row value: %s\n", row[0]);
+    return EXIT_FAILURE;
+  }
+  row = drizzle_row_index(result, 1);
+  if (row == NULL)
+  {
+    printf("Could not get indexed row\n");
+    return EXIT_FAILURE;
+  }
+  if (strcmp(row[0], "2") != 0)
+  {
+    printf("Retrieved bad indexed row value: %s\n", row[0]);
+    return EXIT_FAILURE;
+  }
+  if (drizzle_row_current(result) != 2)
+  {
+    printf("Index at wrong pos\n");
+    return EXIT_FAILURE;
+  }
+  size_t *sizes= drizzle_row_field_sizes(result);
+  if (sizes[0] != 1)
+  {
+    printf("Row size mismatch (4 != %lu)\n", sizes[0]);
+    return EXIT_FAILURE;
+  }
+
+  drizzle_result_free(result);
+
+  drizzle_query_str(con, "drop table libdrizzle.t1", &ret);
+  if (ret != DRIZZLE_RETURN_OK)
+  {
+    printf("Drop table failure\n");
+    return EXIT_FAILURE;
+  }
+
+
+  drizzle_quit(con);
+  return EXIT_SUCCESS;
+}
