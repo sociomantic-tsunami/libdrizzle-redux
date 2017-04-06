3ddffe0b28f446a30218f836b90d915e45481f0b
Andreas Bok Andersen <andreas.bok@sociomantic.com>
2017-04-06 14:44:35 +0200

    Remove after-install script from deb packaging
    The symlink introduced in
    f26848e7cbc317d090aa74e5824c799b62ef92c1
    is no longer needed after installation

--- deb/after_install.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-#
-# Script which is run after install to ensure binary compatibility between
-# this and the previous version of libdrizzle-redux
-
-sudo ln -sf /usr/lib/libdrizzle-redux.so.10 /usr/lib/libdrizzle-redux.so.9
