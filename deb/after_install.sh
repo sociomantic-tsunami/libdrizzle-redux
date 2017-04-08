e8c497a5cba1d8128bbc3924f88cf579f9cce66e
Andreas Bok Andersen <andreas.bok@sociomantic.com>
2017-04-08 15:35:59 +0200

    Fix: remove outdated symlink for libdrizzle-redux.so.10
    The symlink part of the v5.4.0 release to not break
    binary compatibility
    It is no longer needed in v6.0.0

--- deb/after_install.sh
+++ deb/after_install.sh
@@ -3,4 +3,11 @@
 # Script which is run after install to ensure binary compatibility between
 # this and the previous version of libdrizzle-redux
 
-sudo ln -sf /usr/lib/libdrizzle-redux.so.10 /usr/lib/libdrizzle-redux.so.9
+if [ -h /usr/lib/libdrizzle-redux.so.9 ]; then
+    echo "Removing outdated symlink"
+    sudo rm /usr/lib/libdrizzle-redux.so.10 /usr/lib/libdrizzle-redux.so.9 2>/dev/null
+    if [ -e "/usr/lib/libdrizzle-redux.so.9" ]; then
+        echo "Couldn't remove symlink '/usr/lib/libdrizzle-redux.so.9'"
+              "Please remove it manually"
+    fi;
+fi;
