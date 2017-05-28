d06c2abad3c86226fbda0ebdea2c2f614918095d
Andreas Bok Andersen <andreas.bok@sociomantic.com>
2017-05-28 12:49:52 +0200

    Fix invalid parsing of MYSQL_PORT env variable
    Fixes a bug where the env variable MYSQL_PORT used
    as connection parameter in the unittests was checked
    for but not read correctly.

--- tests/unit/statement.c
+++ tests/unit/statement.c
@@ -53,7 +53,7 @@ int main(int argc, char *argv[])
   ASSERT_EQ(UINT64_MAX, drizzle_stmt_row_count(stmt));
 
   drizzle_st *con = drizzle_create(getenv("MYSQL_SERVER"),
-                                   getenv("MYSQL_PORT") ? atoi("MYSQL_PORT")
+                                   getenv("MYSQL_PORT") ? atoi(getenv("MYSQL_PORT"))
                                                         : DRIZZLE_DEFAULT_TCP_PORT,
                                    getenv("MYSQL_USER"),
                                    getenv("MYSQL_PASSWORD"),
