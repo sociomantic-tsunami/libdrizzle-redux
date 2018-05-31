54f7cca7eb77f1051d6873d0da5e506b914672dd
Andreas Bok Andersen <andreas.bok@sociomantic.com>
2018-05-31 16:27:07 +0200

    Add distribution specific container scripts
    Reduce the complexity of the existing container_entrypoint.sh
    by moving the distribution specific parts into separate
    scripts.
    
    Which script is copied to the container depends on the
    $DIST_NAME environment variable

--- docker/container_entrypoint.sh
+++ docker/container_entrypoint.sh
@@ -1,33 +1,14 @@
 #!/bin/bash
 #
-# entrypoint for the container
+# common entrypoint for the container
+
+COMPILER_VERSION=${COMPILER_VERSION:+-$COMPILER_VERSION}
+
+source "entrypoint.$DIST_NAME.sh"
 
 # set and export the C and C++ compiler environment variables
-export CXX=$CXX${COMPILER_VERSION:+-$COMPILER_VERSION}
-export CC=$CC${COMPILER_VERSION:+-$COMPILER_VERSION}
-
-install_dependencies()
-{
-    if [[ "$DIST_NAME" == "ubuntu" ]]; then
-        # install compiler
-        COMPILER_PACKAGE=$CXX
-        if [[ "$CC" =~ "clang" ]]; then
-            COMPILER_PACKAGE=$CC
-        fi
-
-        apt-fast install -y --no-install-recommends $COMPILER_PACKAGE
-    fi
-
-    if [[ "$DIST_NAME" == "centos" ]]; then
-        # install compiler
-        if [[ "$CC" =~ "clang" ]]; then
-            yum install -y clang
-        fi
-    fi
-}
-
-# install dependencies
-install_dependencies
+export CXX=$CXX$COMPILER_VERSION
+export CC=$CC$COMPILER_VERSION
 
 # configure and compile the project
 # run make targets specified in the environment variable MAKE_TARGET
@@ -37,7 +18,9 @@ cd build
 
 if [[ ! -e Makefile ]]; then
     autoreconf -fi ..
-    ../configure
+    ../configure ${CONFIGURE_ARGS}
 fi
 
 make ${MAKE_TARGET//:/ }
+
+ls -la
