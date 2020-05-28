ca684c1ac7b5a41c54303440d4504c41c07e6219
Andreas Bok Andersen <bok.chan@gmail.com>
2020-05-28 21:39:15 +0200

    Upgrade travis build config
    - Remove outdated ci jobs on outdated versions of ubuntu and centos
    - Use ubuntu 20.04 (focal) and centos 8
    - Update docker-compose files and dockerfiles

--- docker/entrypoint.centos.sh
+++ docker/entrypoint.centos.sh
@@ -2,26 +2,6 @@
 #
 # entrypoint for the container running centos
 
-install_yum_repo()
-{
-    if ! type "$CC" &> /dev/null; then
-        if [[ "$CC" =~ "clang" && -n "$COMPILER_VERSION" ]]; then
-        echo "\
-[alonid-llvm-$COMPILER_VERSION.0]
-name=Copr repo for llvm-$COMPILER_VERSION.0 owned by alonid
-baseurl=https://copr-be.cloud.fedoraproject.org/results/alonid/llvm-$COMPILER_VERSION.0/epel-7-\$basearch/
-type=rpm-md
-skip_if_unavailable=True
-gpgcheck=1
-gpgkey=https://copr-be.cloud.fedoraproject.org/results/alonid/llvm-$COMPILER_VERSION.0/pubkey.gpg
-repo_gpgcheck=0
-enabled=1
-enabled_metadata=1
-" >> /etc/yum.repos.d/epel.repo
-        fi
-    fi
-}
-
 install_dependencies()
 {
     # install compiler
@@ -29,7 +9,6 @@ install_dependencies()
 
     # install compiler
     if [[ "$CC" =~ "clang" ]]; then
-        install_yum_repo
         yum install -y COMPILER_PACKAGE
         export PATH=/opt/llvm-$COMPILER_VERSION.0/bin:$PATH
     fi
@@ -37,5 +16,3 @@ install_dependencies()
 
 # install dependencies
 install_dependencies
-
-
